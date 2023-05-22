49c7ccd65b7b6ca49c4c5d4f244d68fcccae03ab
Taha dlrb <tahadlrb7@gmail.com>
2023-05-22 23:04:03 +0330

    Add some colorscheme

--- lua/dinno/core/colorscheme.lua
+++ lua/dinno/core/colorscheme.lua
@@ -1,6 +1,11 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+
+-- installed themeS
+-- dark : nightfly / nightfox / carbonfox / terafox / nordfox / duskfox / catppuccin-mocha / catppuccin-frappe / catppuccin-macchiato
+-- light : dawnfox / dayfox / catppuccin-latte
+--
+local status, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
